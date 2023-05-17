4e1af8d6fa95b2ed2a08de7b4a2fabbc29f6aebc
soriful15 <sorifulislam.rs10@gmailcomm>
2023-05-17 21:39:38 +0600

    npm i and tailwind and daysui install

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: { browser: true, node: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended'
+
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/no-unescaped-entities": 0,
+    "react-hooks/exhaustive-deps": 0
+  },
+}
+
